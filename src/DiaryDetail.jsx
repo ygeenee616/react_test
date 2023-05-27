eeac9e31809d12a109591260cc3b0a6751a4e5e1
남연진 <ygeenee@naver.com>
2023-05-27 11:39:10 +0900

    navigate

--- src/DiaryDetail.jsx
+++ src/DiaryDetail.jsx
@@ -1,13 +1,40 @@
 import { useLocation, useNavigate } from "react-router-dom";
+import { diaries } from "./database";
 
 function DiaryDetail() {
   const navigate = useNavigate();
   const location = useLocation();
+  const diaryNum = parseInt(location.pathname.split("/")[2]);
+  const title = diaries[diaryNum].t;
+  const contents = diaries[diaryNum].c;
+
+  const goBack = () => {
+    navigate("/diary");
+  };
+
+  const goHome = () => {
+    navigate("/");
+  };
 
   return (
     <div>
-      <h4>{location.pathname.split("/")}</h4>
-      <button>뒤로 가기</button>
+      <h4>{}</h4>
+      <h1>제목: {title}</h1>
+      <h3>오늘의 일기</h3>
+      <div
+        style={{
+          width: "70vw",
+          height: 200,
+          margin: 10,
+          padding: 10,
+          backgroundColor: "lightblue",
+        }}
+      >
+        {contents}
+      </div>
+
+      <button onClick={goHome}>홈으로</button>
+      <button onClick={goBack}>뒤로 가기</button>
     </div>
   );
 }
